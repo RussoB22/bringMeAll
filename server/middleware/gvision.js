8c509de531a21958a731cebb158cd83645cce915
RussoB22 <russo.borneo2013@gmail.com>
2023-07-28 03:54:09 -0500

    logs

--- server/middleware/gvision.js
+++ server/middleware/gvision.js
@@ -7,18 +7,20 @@ const Room = require('../models/Rooms');
 const Players = require('../models/Players');
 const hostServer = process.env.HOSTSERVER;
 
-const credentials = {
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECT_ID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY,
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
-};
+// const credentials = {
+//   "type": process.env.TYPE,
+//   "project_id": process.env.PROJECT_ID,
+//   "private_key_id": process.env.PRIVATE_KEY_ID,
+//   "private_key": process.env.PRIVATE_KEY,
+//   "client_email": process.env.CLIENT_EMAIL,
+//   "client_id": process.env.CLIENT_ID,
+//   "auth_uri": process.env.AUTH_URI,
+//   "token_uri": process.env.TOKEN_URI,
+//   "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+//   "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+// };
+
+const credentials = process.env.CREDENTIALS
 
 
 
